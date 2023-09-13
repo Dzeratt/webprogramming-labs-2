9c6d55de34d240b3b17347d7a4ebe6fd85466914
Духов Денис <denishomework@mail.ru>
2023-09-13 16:57:33 +0700

    Меню

--- app.py
+++ app.py
@@ -1,8 +1,36 @@
 from flask import Flask
 app = Flask(__name__)
 @app.route("/")
+@app.route("/index")
 def start():
     return """
+    <!doctype html>
+    <html>
+        <head>
+            <title>НГТУ, ФБ, Лабораторные работы</title>
+        </head>
+        <body>
+            <header>
+                НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+            </header>
+
+            <main>
+                <ol>
+                <li>
+                    <a href="/lab1" target="_blank">Лабораторная работа 1</a>
+                </li>
+                <ol>
+            </main>
+
+            <footer>
+                &copy; Духов Д.Ю., ФБИ-13, 3 курс, 2023
+            </footer>
+        </body>
+    </html>    
+"""
+@app.route("/lab1")
+def lab1():
+        return """
     <!doctype html>
     <html>
         <head>
@@ -14,6 +42,14 @@ def start():
             </header>
 
             <h1>web-сервер на flask</h1>
+
+            <div>
+                Flask — фреймворк для создания веб-приложений на языке
+                программирования Python, использующий набор инструментов
+                Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
+                называемых микрофреймворков — минималистичных каркасов
+                веб-приложений, сознательно предоставляющих лишь самые базовые возможности.
+            </div>
 
             <footer>
                 &copy; Духов Д.Ю., ФБИ-13, 3 курс, 2023
