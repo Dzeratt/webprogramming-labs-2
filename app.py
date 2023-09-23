eb33cab7a85eb0657e7dae1232eed102c001d3fa
Духов Денис <denishomework@mail.ru>
2023-09-24 00:17:43 +0700

    цикл в шаблоне

--- app.py
+++ app.py
@@ -188,8 +188,12 @@ def something():
 '''
 @app.route('/lab2/example')
 def example():
-     name = 'Денис Духов'
-     lab_number = '2'
-     group = 'ФБИ-13'
-     course = '3 курс'
-     return render_template('example.html', name=name, lab_number = lab_number, group =group, course=course)
+     name, lab_number, group, course = 'Денис Духов', 2, 'ФБИ-13', '3 курс'
+     fruits = [
+               {'name': 'яблоки', 'price': 100},
+               {'name': 'груши', 'price': 120},
+               {'name': 'апельсины', 'price': 80},
+               {'name': 'мандарины', 'price': 95},
+               {'name': 'манго', 'price': 321}
+    ]
+     return render_template('example.html', name=name, lab_number = lab_number, group =group, course=course, fruits=fruits)
