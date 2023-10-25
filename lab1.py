3300d30deda2325ab3f6a48bf6efab24d0148cf0
Духов Денис <denishomework@mail.ru>
2023-10-25 16:24:15 +0700

    Заготовка к 4-ой лабораторной

--- lab1.py
+++ lab1.py
@@ -33,6 +33,9 @@ def menu():
                         <li>
                             <a href="/lab3/" target="_blank">Лабораторная работа 3</a>
                         </li>
+                        <li>
+                            <a href="/lab4/" target="_blank">Лабораторная работа 4</a>
+                        </li>
                     <ol>
                 </main>
 
