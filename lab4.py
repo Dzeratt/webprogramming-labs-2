9c91f09f13731ec2816a133699363208157a22c5
Духов Денис <denishomework@mail.ru>
2023-11-08 14:12:09 +0700

    куки - цвет текста

--- lab4.py
+++ lab4.py
@@ -86,4 +86,14 @@ def order_grain():
 
     return render_template('ordergrain.html')
 
-
+@lab4.route('/lab4/cookies', methods=['GET', 'POST'])
+def cookies():
+    if request.method == "GET":
+        return render_template('cookies.html')
+    
+    color = request.form.get('color')
+    headers = {
+        'Set-Cookie': 'color=' + color + '; path=/',
+        'Location': '/lab4/cookies'
+    }
+    return '', 303, headers
