7a59ca72d7c13bad7b43c5c12c458a0d9ada3df1
Духов Денис <denishomework@mail.ru>
2023-12-06 14:51:06 +0700

    Получение информации о курсе/-ах по REST

--- lab7.py
+++ lab7.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,render_template, request, redirect, session
+from flask import Blueprint,render_template, request, redirect, session, abort
 
 lab7 = Blueprint('lab7', __name__)
 
--- lab8.py
+++ lab8.py
@@ -1,7 +1,25 @@
-from flask import Blueprint,render_template, request, redirect, session
+from flask import Blueprint,render_template, request, redirect, session, abort
 
 lab8 = Blueprint('lab8', __name__)
 
 @lab8.route('/lab8/')
 def main():
     return render_template('/lab8/index.html')
+
+courses = [
+    {'name': 'c++', 'videos':3, 'price': 3000},
+    {'name': 'basic', 'videos':30, 'price': 0},
+    {'name': 'c#', 'videos':8}
+]
+
+
+@lab8.route('/lab8/api/courses/', methods=["GET"])
+def get_courses():
+    return courses
+
+@lab8.route('/lab8/api/courses/<int:course_num>', methods=["GET"])
+def get_course(course_num):
+    if 0 <= course_num < len(courses):
+        return courses[course_num]
+    else:
+        abort(404)
