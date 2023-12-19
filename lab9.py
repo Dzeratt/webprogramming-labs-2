9b829053195477576768c96450e622befee760d5
Духов Денис <denishomework@mail.ru>
2023-12-20 05:30:18 +0700

    обработчик ошибки 500

--- lab9.py
+++ lab9.py
@@ -9,3 +9,12 @@ def main():
 @lab9.app_errorhandler(404)
 def not_found(e):
     return render_template('/lab9/error.html'), 404
+
+@lab9.app_errorhandler(500)
+def internal_server_error(error):
+    return render_template('/lab9/error500.html'), 500
+
+@lab9.route('/lab9/500')
+def trigger_error():
+    a = 1 / 0
+    return 'This will not be executed'
